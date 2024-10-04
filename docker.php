fdbed0bf44890609925ced9dd0a470ea7eaa998b
Moreno Razzoli <morrolinux@gmail.com>
2024-10-04 16:15:23 +0200

    add docker stats and reviews

--- docker.php
+++ docker.php
@@ -2,6 +2,8 @@
 <html lang="it">
 
 <head>
+  <script defer src="https://analytics.morrolinux.it/script.js" data-website-id="c3d595bf-aa00-4953-8d69-5e3b8878793a"></script>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -22,6 +24,16 @@
 <body id="page-top">
 
   <?php
+
+    //UDEMY STATS
+    ini_set("allow_url_fopen", 1);
+
+    $json = file_get_contents('https://www.udemy.com/api-2.0/courses/6190731?fields[course]=title,num_subscribers,num_lectures,num_reviews');
+    $DPCM_STATS = json_decode($json);
+
+    $subs = $DPCM_STATS->num_subscribers;
+    $reviews = $DPCM_STATS->num_reviews;
+    $lessons = $DPCM_STATS->num_lectures;
 
     // SHOW PROMO BANNER?
     // promo.json example: {"promo_str": "Offerta a tempo limitato!<br>Tutti i corsi in <b>offerta speciale</b> a 9,99 €<br>termina venerdì 8 marzo", "expire_date": "07-03-2024 23:59:59"}
@@ -72,16 +84,15 @@
             <a data-umami-event="docker_navbar_corsi" class="nav-link js-scroll-trigger" href="#docker">Docker</a>
           </li>
           <li class="nav-item">
+            <a data-umami-event="docker_navbar_recensioni" class="nav-link js-scroll-trigger" href="#recensioni">Recensioni</a>
+          </li>
+          <li class="nav-item">
             <a data-umami-event="docker_navbar_faq" class="nav-link js-scroll-trigger" href="#corsi">Corsi</a>
           </li>
           <li class="nav-item">
             <a data-umami-event="docker_navbar_faq" class="nav-link js-scroll-trigger" href="#faq">FAQ</a>
           </li>
-          <!--
-          <li class="nav-item">
-            <a data-umami-event="docker_navbar_recensioni" class="nav-link js-scroll-trigger" href="#recensioni">Recensioni</a>
-          </li>
-          -->
+
           <li class="nav-item">
             <a data-umami-event="docker_navbar_docente" class="nav-link js-scroll-trigger" href="#docente">Docente</a>
           </li>
@@ -162,7 +173,6 @@
 
   </section>
 
-<!--
   <section id="recensioni">
     <div class="text-center">
       <h2>Fidati di chi l'ha già acquistato</h2>
@@ -172,13 +182,11 @@
           <div class="carousel-item active">
             <p class="review-body">
               <i class="fa fa-quote-left" aria-hidden="true"></i>
-              È stato un video corso molto utile nello studio contestuale della guida ufficiale. <br>
-              Con il voucher fornito da Moreno ho usufruito di un piccolo sconto <br>
-              per sostenere l’esame, che è andato alla grande. 
-              Consigliato.              
+              Tutti nell'Internet sanno già che Morro è molto bravo a spiegare le cose e anche questa volta non si smentisce.<br> 
+              Il contenuto del corso è molto utile nei casi reali.
               <i class="fa fa-quote-right" aria-hidden="true"></i>
             </p>
-            <p class="review-name"><i>Chiara, studentessa "Linux Essentials"</i></p>
+            <p class="review-name"><i>Michele, studente "Docker Per Comuni Mortali"</i></p>
             <div class="stars">
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
@@ -191,11 +199,11 @@
           <div class="carousel-item">
             <p class="review-body">
               <i class="fa fa-quote-left" aria-hidden="true"></i>
-              Ottimo Corso!! Grazie Moreno di tutto.
-              Ora ho capito e fatto mie tante nozioni che prima mi erano ostiche!!
+              Corso molto interessante. Cercavo una panoramica su Docker ma qui trovo molto di più di quello che mi serve.<br>
+              Complimenti anche al docente che è davvero molto preparato ed espone con una giusta tempistica.
               <i class="fa fa-quote-right" aria-hidden="true"></i>
             </p>
-            <p class="review-name"><i>Roberto, studente "LPIC 101"</i></p>
+            <p class="review-name"><i>Stefano, studente "Docker Per Comuni Mortali"</i></p>
             <div class="stars">
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
@@ -208,10 +216,10 @@
           <div class="carousel-item">
             <p class="review-body">
               <i class="fa fa-quote-left" aria-hidden="true"></i>
-              Riesce a far comprendere in maniera semplice argomenti difficili. <br>Corso completo. Dà sempre una risposta esauriente ai quesiti posti.
+              Tanta qualità in ogni aspetto. C’è molto lavoro personale nel esporre determinati argomenti rendendoli semplici ma allo stesso tempo rimanendo professionale
               <i class="fa fa-quote-right" aria-hidden="true"></i>
             </p>
-            <p class="review-name"><i>Michele, studente "LPIC 102"</i></p>
+            <p class="review-name"><i>Giuseppe, studente "Docker Per Comuni Mortali"</i></p>
             <div class="stars">
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
@@ -224,11 +232,12 @@
           <div class="carousel-item">
             <p class="review-body">
               <i class="fa fa-quote-left" aria-hidden="true"></i>
-              Docente preparatissimo e molto simpatico! Ho imparato molto senza annoiarmi un secondo!<br>
-              Corso consigliatissimo per chi, come me, parte da zero!
+              Ringrazierò sempre moreno, grazie alla sua passione e alla sua dote innata nello spiegare 
+              in modo semplice, conciso ed efficace ho potuto inserirmi in un contesto lavorativo estremamente innovativo!
+              ho comprato tutti i suoi corsi e comprerò qualsiasi suo corso in futuro!
               <i class="fa fa-quote-right" aria-hidden="true"></i>
             </p>
-            <p class="review-name"><i>Luca, studente "Arch Linux Per Comuni Mortali"</i></p>
+            <p class="review-name"><i>Ivan, studente "Docker Per Comuni Mortali"</i></p>
             <div class="stars">
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
@@ -241,10 +250,10 @@
           <div class="carousel-item">
             <p class="review-body">
               <i class="fa fa-quote-left" aria-hidden="true"></i>
-              Ho trovato un sacco di cose che non sapevo e adesso ho tante idee per la testa che cercherò di realizzare. Grazie Moreno, è un corso davvero ben fatto.
+              Spiegazione lineare e comprensibile. Animazioni che aiutano la comprensione
               <i class="fa fa-quote-right" aria-hidden="true"></i>
             </p>
-            <p class="review-name"><i>Dog, studente "LPIC 102"</i></p>
+            <p class="review-name"><i>610v4nn1, studente "Docker Per Comuni Mortali"</i></p>
             <div class="stars">
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
@@ -257,11 +266,10 @@
           <div class="carousel-item">
             <p class="review-body">
               <i class="fa fa-quote-left" aria-hidden="true"></i>
-              Il corso è stato splendido, divertente e anche molto più interessante di quanto mi aspettassi.<br>
-              Questo perchè c'è passione in chi spiega e si nota! Penso proprio che continuerò con i prossimi corsi Linux :D
+              Morro si conferma un insegnate fantastico; chiaro, preciso e con la capacità di tenere alta l'attenzione.
               <i class="fa fa-quote-right" aria-hidden="true"></i>
             </p>
-            <p class="review-name"><i>Marco, studente "Linux Essentials"</i></p>
+            <p class="review-name"><i>Alessio, studente "Docker Per Comuni Mortali"</i></p>
             <div class="stars">
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
@@ -274,12 +282,10 @@
           <div class="carousel-item">
             <p class="review-body">
               <i class="fa fa-quote-left" aria-hidden="true"></i>
-              Consiglio questo corso e lo metto nei preferiti. Morro ti trasmette la sua passione per Linux. <br>
-              Il corso è conforme al 100% agli argomenti d'esame, e non solo, ogni tanto ti regala dei fuori programma.<br>
-              P.S. l'esame l'ho passato.
+              Corso molto esaustivo per il primo approccio ai container. Il docente è molto preparato e coinvolgente nella spiegazione.
               <i class="fa fa-quote-right" aria-hidden="true"></i>
             </p>
-            <p class="review-name">Paolo, studente "LPIC 101"</p>
+            <p class="review-name">Enrico, studente "Docker Per Comuni Mortali"</p>
             <div class="stars">
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
@@ -292,12 +298,12 @@
           <div class="carousel-item">
             <p class="review-body">
               <i class="fa fa-quote-left" aria-hidden="true"></i>
-              Ottimo corso, piacevole da seguire. Morro ha una grande capacità divulgativa che si adatta<br>
-               ad utenti con esperienza e a chi come me di esperienza ne ha da fare. <br>
-              Mai troppo prolisso o troppo conciso, è l'equilibrio che caratterizza questo percorso: grazie Moreno.
+              Moreno è un istruttore fantastico, spiega concetti complessi rendendoli chiari e fruibili, il tutto in modo piacevole e accogliente.<br> 
+              Devo tutto a lui la mia passione per l'Open Source e Linux. Il primo step della LPIC essentials l'ho passato grazie al suo corso, <br>
+              il migliore tra tutti quelli trovati in rete.
               <i class="fa fa-quote-right" aria-hidden="true"></i>
             </p>
-            <p class="review-name"><i>Alessandro, studente "Arch Linux Per Comuni Mortali"</i></p>
+            <p class="review-name"><i>Patrizio, studente "Docker Per Comuni Mortali"</i></p>
             <div class="stars">
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
               <i class="fa fa-star fa-lg" aria-hidden="true"></i>
@@ -319,7 +325,6 @@
       </div>
     </div>
   </section>
--->
   
 <!--
   <section id="lavoro" style="margin-top: -3rem;">
@@ -363,11 +368,29 @@
       </div>
     </div>
   </section>
+-->
 
-  <section data-umami-event="docker_section_statistiche" id="statistiche">
+  <section id="batterie" style="margin-top: -3rem;">
     <div class="container text-center">
+      <h2 class="my-5">BATTERIE INCLUSE</h2>
+      <p class="mb-2 text-justify">
+
+      Qui imparerai tutto ciò che c'è da sapere su Docker e l'ecosistema dei <i>container OCI</i> tramite <b>spiegazioni semplici</b> ed esempi.
+      <br><br>
+
+      Esploreremo Docker con un <b>approccio pratico</b>: dopo aver spiegato le basi, <b>ogni lezione sarà costruita intorno a un mini progetto funzionante</b>, e ti illustrerò ogni dettaglio rilevante strada facendo.<br><br>
+
+      Allegato a ciascuna lezione troverai <b>codice</b> e risorse utilizzate, <b>appunti riassuntivi</b> e link di approfondimento, e potrai verificare il tuo apprendimento tramite gli appositi <b>quiz di fine capitolo</b>.<br><br>
+
+      A differenza dei corsi più avanzati, il focus è sull'accessibilità e la comprensione. <i>Il tono è discorsivo e rilassato</i>, ogni argomento viene trattato quando più opportuno e approfondito successivamente negli appositi capitoli.
+      <br><br>
+      </p>
+  </section>
+
+  <section data-umami-event="docker_section_statistiche" id="statistiche" style="background-color: #2495ed">
+    <div class="container text-center">
+    <h2>Numeri che parlano<br></h2>
       <p class="mb-5">
-        Unisciti agli oltre novemila studenti già iscritti al <b>corso bestseller italiano su Linux</b>, costruisci solide basi e <b>inizia la tua carriera come professionista in un settore in rapida crescita!</b>
       </p>
       <div id="counters" class="row">
         <div class="col-lg-4 stats">
@@ -390,28 +413,8 @@
     </div>
   </section>
 
--->
-
-
-  <section id="batterie" style="margin-top: -9rem;">
-    <div class="container text-center">
-      <h2 class="my-5">BATTERIE INCLUSE</h2>
-      <p class="mb-2 text-justify">
-
-      Qui imparerai tutto ciò che c'è da sapere su Docker e l'ecosistema dei <i>container OCI</i> tramite <b>spiegazioni semplici</b> ed esempi.
-      <br><br>
-
-      Esploreremo Docker con un <b>approccio pratico</b>: dopo aver spiegato le basi, <b>ogni lezione sarà costruita intorno a un mini progetto funzionante</b>, e ti illustrerò ogni dettaglio rilevante strada facendo.<br><br>
-
-      Allegato a ciascuna lezione troverai <b>codice</b> e risorse utilizzate, <b>appunti riassuntivi</b> e link di approfondimento, e potrai verificare il tuo apprendimento tramite gli appositi <b>quiz di fine capitolo</b>.<br><br>
-
-      A differenza dei corsi più avanzati, il focus è sull'accessibilità e la comprensione. <i>Il tono è discorsivo e rilassato</i>, ogni argomento viene trattato quando più opportuno e approfondito successivamente negli appositi capitoli.
-      <br><br>
-      </p>
-  </section>
-
   <section id="corsi">
-    <div class="container" style="margin-top: -9rem;">
+    <div class="container" style="margin-top: 0rem;">
       <h2 class="mb-5 text-center">Scopri tutti i corsi Per Comuni Mortali</h2>
       <p class="mb-2 text-justify">
         La linea "Per Comuni Mortali" è pensata per <b>rendere accessibili a tutti anche i concetti più tecnici.</b> <br>
@@ -654,16 +657,9 @@
   <script src="js/jquery.fitvids.js"></script>
 
   <!-- Custom JavaScript -->
-  <script src="js/script.js"></script>
+  <script src="js/docker_script.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
 
-  <script type="text/javascript">
-            document.getElementById("altre_testimonianze").style.display = "none";
-            function hideMyDiv(){
-              document.getElementById("altre_testimonianze").style.display = "block";
-              document.getElementById("testimonials_load_more").style.display = "none";
-            }
-  </script>
   
 </body>
 
---
+++ js/docker_script.js
@@ -0,0 +1,199 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 56)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+})(jQuery); // End of use strict
+
+function isInViewport(el, partially) {
+
+  if (typeof(partially) === 'undefined') {
+    var partially = 0;
+  }
+
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+
+  var rect = el.getBoundingClientRect();
+  var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+  var windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+  partialCoeff = 1;
+
+  if (partially == 1) {
+    var vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    var horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+    return (vertInView && horInView);
+  } else if (partially > 1)
+    partialCoeff = partially;
+
+  return ((rect.left >= 0) && (rect.top >= 0) && 
+  ((rect.left + rect.width) <= windowWidth) && 
+  ((rect.top + rect.height/partialCoeff) <= windowHeight));
+}
+
+
+function increment(){
+  $('.counting').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+    {
+      duration: 1000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+      }
+    });
+  });
+}
+
+
+function drawChart(){
+  // create and animate charts only when they become visible
+  // loop over all html-defined doughnut charts
+  var charts = document.getElementsByClassName("doughnut");
+  for (var i = 0; i < charts.length; i++) {
+    var canva = document.createElement("canvas");
+    var desc = charts[i].getAttribute("desc");
+    var inputColor = charts[i].getAttribute("color");
+    var inputValue = charts[i].getAttribute("value");
+    charts[i].appendChild(canva);
+
+    // set data
+    data = {
+      datasets:
+      [{
+        data: [inputValue, 100-inputValue],
+        backgroundColor: [inputColor,"#FFFFFF00"],
+      }],
+    }
+  
+    // create chart
+    var promisedDeliveryChart = new Chart(canva, {
+      type: 'doughnut',
+      data: data,
+      options: {
+        elements: {arc: {borderWidth: 0}},
+        cutoutPercentage: 80,
+        tooltips: {enabled: false},
+        hover: {mode: null},
+        responsive: true,
+        legend: {display: false}
+      }
+    });
+
+    // override chart with percentage value
+    Chart.plugins.register({
+      id: desc,
+      beforeDraw: function(chart) {
+        var width = chart.chart.width,
+            height = chart.chart.height,
+            ctx = chart.chart.ctx;
+        ctx.restore();
+        var fontSize = (height / 114).toFixed(2);
+        ctx.font = fontSize + "em sans-serif";
+        ctx.textBaseline = "middle";
+        var text = chart.data.datasets[0]["data"][0] + "%",
+            textX = Math.round((width - ctx.measureText(text).width) / 2),
+            textY = height / 2;
+        ctx.fillText(text, textX, textY);
+        ctx.save();
+      }
+    });
+
+    // append the chart to the DOM
+    var description = document.createElement("p");
+    var spacer = document.createElement("br");
+    var h5 = document.createElement("h5");
+    description.appendChild(spacer);
+    description.appendChild(h5);
+    h5.innerHTML = desc;
+    charts[i].appendChild(description);
+  }
+}
+
+var recensioniVisible = false;
+var testimonianzeVisible = false;
+var percorsoVisible = false;
+var comparaVisible = false;
+var LFStatsChartsVisible = false;
+var countersVisible = false;
+
+function track_scroll(){
+  if(isInViewport($('#recensioni'), 1)){
+    if(recensioniVisible) return;
+    recensioniVisible = true;
+    umami.track('scroll_recensioni');
+    return;
+  }
+  if(isInViewport($('#counters'), 1)){
+    if(countersVisible) return;
+    countersVisible = true;
+    increment();
+    umami.track('scroll_counters');
+    return;
+  }
+  if(isInViewport($('#testimonianze'), 1)){
+    if(testimonianzeVisible) return;
+    testimonianzeVisible = true;
+    umami.track('scroll_testimonianze');
+    return;
+  }
+  if(isInViewport($('#percorso'), 1)){
+    if(percorsoVisible) return;
+    percorsoVisible = true;
+    umami.track('scroll_percorso');
+    return;
+  }
+  if(isInViewport($('#compara'), 1)){
+    if(comparaVisible) return;
+    comparaVisible = true;
+    umami.track('scroll_compara');
+    return;
+  }
+}
+
+
+$('.yt_video').fitVids();
+
+
+$(window).scroll(function(){
+  track_scroll();
+});
+
+$(document).ready(function(){
+  track_scroll();
+  // increment();
+  drawChart();
+});
